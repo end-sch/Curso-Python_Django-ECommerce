b1c7b1efd5f15bb67f5d98631f93bcd1792a8d5a
end-sch <evandroscholl@hotmail.com>
2022-04-30 19:51:59 -0300

    Finalizado pedido, e correção de bugs

--- pedido/urls.py
+++ pedido/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 
-app_name = 'Pedido'
+app_name = 'pedido'
 
 urlpatterns = [
     path('', views.Pagar.as_view(), name="pagar"),
     path('salvarpedido/', views.SalvarPedido.as_view(), name="salvarpedido"),
     path('detalhe', views.Detalhe.as_view(), name="detalhe"),
-
+    path('pagar', views.Pagar.as_view(), name="pagar"),
 ]
